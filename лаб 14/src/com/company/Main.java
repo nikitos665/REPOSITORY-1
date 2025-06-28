f2708c096daf414762ac085ca865afcc1b2dca2f
nikitos665 <trolo2020@mail.ru>
2025-06-28 12:22:56 +0300

    Add files via upload

---
+++ лаб 14/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        String aparat = "номерок мой блатной пацаны 15.255.245.195 салам пацаны гуляем лялелеля 270.65.31.200 лорнер 207.3.58.105 ktktkt 205.60.лингангу 290.897.34 79.34. 255.255.255.255";
+        Pattern pattern = Pattern.compile("(\\b((1\\d{0,2}.)|(2[0-4]\\d.)|(25[0-5].)|([1-9]\\d.)|(\\d.)|(\\d{2}.)){3}((1\\d{0,2})|(2[0-4]\\d)|(25[0-5])|([1-9]\\d)$|(\\d)$))");
+        Matcher matcher = pattern.matcher(aparat);
+        while (matcher.find()){
+            System.out.println(matcher.group());
+    }
+}}
